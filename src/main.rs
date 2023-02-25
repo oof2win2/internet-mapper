d8ac84fa6e782678936165182d5a388569092889
oof2win2 <honza.koco44@gmail.com>
2023-02-25 07:05:02 +0000

    feat: map X.X.0.0 IPs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 extern crate futures;
 extern crate tokio;
 extern crate tokio_icmp_echo;
-use crate::futures::{future, StreamExt};
+use std::{net::Ipv4Addr, time::Duration};
 
-const AMOUNT_OF_PINGS: usize = 3;
+use crate::futures::{future, StreamExt};
+use futures::FutureExt;
+use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
+
+const AMOUNT_OF_PINGS: usize = 1;
 
 #[derive(Debug)]
 struct PingResult {
-    addr: std::net::IpAddr,
+    addr: Ipv4Addr,
     avg_ping: Option<f64>,
     packet_loss: f64,
 }
 
-async fn get_ip_avg_ping(pinger: tokio_icmp_echo::Pinger, addr: std::net::IpAddr) -> PingResult {
-    let stream = pinger.chain(addr).stream();
+async fn get_ip_avg_ping(pinger: &tokio_icmp_echo::Pinger, addr: Ipv4Addr) -> PingResult {
+    let stream = pinger
+        .chain(std::net::IpAddr::V4(addr))
+        .timeout(Duration::from_millis(20))
+        .stream();
     let mut sum = 0.0;
     let mut received_count = 0;
     stream
@@ -41,11 +48,46 @@ async fn get_ip_avg_ping(pinger: tokio_icmp_echo::Pinger, addr: std::net::IpAddr
     }
 }
 
+async fn ping_block(image: &mut RgbImage, pinger: &tokio_icmp_echo::Pinger, blocks: [u8; 2]) {
+    let mut data_futures = vec![];
+    for b3 in 0..255 {
+        for b4 in 0..255 {
+            // let addr = Ipv4Addr::from([blocks[0], blocks[1], b3, b4]);
+            let addr = Ipv4Addr::from([b3, b4, 0, 0]); // faster, 256x256 image
+            let result = get_ip_avg_ping(pinger, addr);
+            data_futures.push(result);
+        }
+    }
+    let data = futures::future::join_all(data_futures).await;
+    for d in data {
+        match d.avg_ping {
+            Some(res) => {
+                image.put_pixel(
+                    d.addr.octets()[0].into(),
+                    d.addr.octets()[1].into(),
+                    image::Rgb([255, 255, 255]),
+                );
+            }
+            None => {}
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let addr = std::env::args().nth(1).unwrap().parse().unwrap();
+    // create a new image buffer, 255^2 x 255^2
+    // let img: RgbImage = ImageBuffer::new(65025, 65025);
+    let mut img: RgbImage = ImageBuffer::new(255, 255);
 
     let pinger = tokio_icmp_echo::Pinger::new().await.unwrap();
-    let data = get_ip_avg_ping(pinger, addr).await;
-    println!("avg ping: {:?}", data);
+    // for a in 0..2 {
+    //     for b in 0..255 {
+    //         println!("{} {}", a, b);
+    //         ping_block(&mut img, &pinger, [a, b]).await;
+    //     }
+    // }
+    ping_block(&mut img, &pinger, [0, 0]).await;
+    // let data = get_ip_avg_ping(pinger, addr).await;
+    // println!("avg ping: {:?}", data);
+    img.save("test.png").unwrap();
 }
